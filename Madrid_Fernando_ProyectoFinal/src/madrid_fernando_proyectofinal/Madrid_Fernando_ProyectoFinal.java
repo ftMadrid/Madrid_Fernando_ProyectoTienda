832296cfae2e11cd43d7507da39aa6699b6477d9
ftMadrid <fernandomadridcv04@gmail.com>
2025-04-28 14:41:29 -0600

    Update Madrid_Fernando_ProyectoFinal.java
    Finished "Abrir Caja" section process.

--- Madrid_Fernando_ProyectoFinal/src/madrid_fernando_proyectofinal/Madrid_Fernando_ProyectoFinal.java
+++ Madrid_Fernando_ProyectoFinal/src/madrid_fernando_proyectofinal/Madrid_Fernando_ProyectoFinal.java
@@ -9,13 +9,17 @@ public class Madrid_Fernando_ProyectoFinal {
         
         Scanner entrada = new Scanner(System.in);
         
+        double caja = 0, adicion_caja;
         int opcion = 0;
+        String tipo_cliente;
         
         while(opcion != 6) {
             System.out.println("===============================");
-            System.out.println("Supercito Morfly");
+            System.out.println("       Supercito Morfly");
             System.out.println("===============================");
             System.out.println("");
+            System.out.print(String.format("Efectivo en Caja: L.%.2f", caja));
+            System.out.println("\n");
             System.out.println("1. Abrir Caja");
             System.out.println("2. Ventas");
             System.out.println("3. Compras");
@@ -29,8 +33,18 @@ public class Madrid_Fernando_ProyectoFinal {
                 opcion = entrada.nextInt();
                 
                 switch(opcion) {
-                    
                     case 1: // Abrir Caja
+                        System.out.println("===============================");
+                        System.out.println("  Supercito Morfly | Caja");
+                        System.out.println("===============================");
+                        System.out.println("");
+                        System.out.print("Ingrese la cantidad que desea adicionar: ");
+                        adicion_caja = entrada.nextDouble();
+                        caja += adicion_caja;
+                        System.out.println("\n");
+                        System.out.println("**** Efectivo Adicionado Exitosamente ****");
+                        System.out.println("");
+                        break;
                     case 2: // Ventas
                     case 3: // Compras
                     case 4: // Reportes
